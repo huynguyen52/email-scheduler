22e35f8733d213626593c7320a7fdf18318d4504
Linh Nguyen <lnguyen>
2023-03-13 16:26:40 +0700

    implement email job

--- src/main/java/com/example/emailscheduler/quartz/job/EmailJob.java
+++ src/main/java/com/example/emailscheduler/quartz/job/EmailJob.java
@@ -1,5 +1,6 @@
 package com.example.emailscheduler.quartz.job;
 
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
@@ -7,6 +8,15 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
 public class EmailJob extends QuartzJobBean {
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        JobDataMap jobDataMap = context.getMergedJobDataMap();
+
+        String subject = jobDataMap.getString("subject");
+        String email = jobDataMap.getString("email");
+        String body = jobDataMap.getString("body");
+        sendMail();
+    }
+
+    private void sendMail(String fromEmail, String toEmail, String subject, String body) {
 
     }
 }
