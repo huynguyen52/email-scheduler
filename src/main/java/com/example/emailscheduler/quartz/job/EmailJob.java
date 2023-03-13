c207948aa97fee6f2a8c3c457e44ef74f2de8312
Linh Nguyen <lnguyen>
2023-03-13 16:42:55 +0700

    implement sendMail() functionality

--- src/main/java/com/example/emailscheduler/quartz/job/EmailJob.java
+++ src/main/java/com/example/emailscheduler/quartz/job/EmailJob.java
@@ -3,9 +3,24 @@ package com.example.emailscheduler.quartz.job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
+
+@Component
 public class EmailJob extends QuartzJobBean {
+    @Autowired
+    private JavaMailSender mailSender;
+    @Autowired
+    private MailProperties mailProperties;
+
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
         JobDataMap jobDataMap = context.getMergedJobDataMap();
@@ -13,10 +28,21 @@ public class EmailJob extends QuartzJobBean {
         String subject = jobDataMap.getString("subject");
         String email = jobDataMap.getString("email");
         String body = jobDataMap.getString("body");
-        sendMail();
+        sendMail(mailProperties.getUsername(), email, subject, body);
     }
 
     private void sendMail(String fromEmail, String toEmail, String subject, String body) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, StandardCharsets.UTF_8.toString());
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            helper.setFrom(fromEmail);
+            helper.setTo(toEmail);
 
+            mailSender.send(message);
+        } catch (MessagingException ex) {
+            System.out.println(ex);
+        }
     }
 }
